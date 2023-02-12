176e9c2bac5df01536cb71f33c037824953aaaa8
alnoman1995 <alnoman.dit@gmail.com>
2023-02-12 20:23:56 +0600

    all user page complite

--- js/home.js
+++ js/home.js
@@ -14,3 +14,65 @@ window.onclick = function (event) {
     }
   }
 };
+
+// echar js
+var dom = document.getElementById("container");
+var myChart = echarts.init(dom);
+var app = {};
+option = null;
+
+option = {
+  tooltip: {
+    trigger: "item",
+    formatter: "{a} <br/>{b}: {c} ({d}%)",
+  },
+  legend: {
+    orient: "vertical",
+    x: "right",
+    data: ["Total client", "Active", "Online", "Disabled"],
+  },
+  series: [
+    {
+      name: "Total",
+      type: "pie",
+      radius: ["50%", "70%"],
+      avoidLabelOverlap: false,
+      label: {
+        normal: {
+          formatter(v) {
+            let text = v.name;
+            return text.length < 4
+              ? text
+              : `${text.slice(0, 4)}\n${text.slice(4)}`;
+          },
+        },
+        emphasis: {
+          show: true,
+          textStyle: {
+            fontSize: "15",
+            fontWeight: "bold",
+          },
+        },
+      },
+      labelLine: {
+        normal: {
+          show: true,
+        },
+      },
+      data: [
+        { value: 310, name: "Total client" },
+        { value: 300, name: "Active" },
+        { value: 290, name: "Online" },
+        {
+          value: 50,
+          name: "Disabled",
+        },
+      ],
+    },
+  ],
+};
+if (option && typeof option === "object") {
+  myChart.setOption(option, true);
+}
+
+// --table---
